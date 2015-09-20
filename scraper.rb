b197705c58fa923092cfa7ddd3d90572324b2c15
Tony Bowden <tony@tmtm.com>
2015-09-20 09:24:23 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,53 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  h4 = noko.xpath('//h4[span[contains(.,"第五屆立法會議員名單")]]')
+  dls = h4.xpath('following-sibling::dl | following-sibling::h2').slice_before { |e| e.name == 'h2' }.first
+  dls.each do |dl|
+    type = dl.css('dt')
+    dl.css('dd').each do |dd|
+      source = dd.css('a[href*="www.al.gov.mo"]/@href').text
+      data = scrape_person(source).merge({ 
+        term: 9,
+        wikiname__zh: dd.xpath('.//a[not(@class="new")]/@title').text 
+      })
+      # puts data
+      ScraperWiki.save_sqlite([:id, :term], data)
+    end
+  end
+end
+      
+def scrape_person(url)
+  noko = noko_for(url)
+  data = { 
+    id: File.basename(url, '.*').gsub('%20','_'),
+    name__zh: noko.xpath('//table//tr[contains(.,"中文姓名")]/td[2]/span').text,
+    name__en: noko.xpath('//table//tr[contains(.,"葡文姓名")]/td[2]/font').text.tidy,
+    image: noko.xpath('//table//tr[contains(.,"中文姓名")]//img/@src').text,
+    term: 9,
+    source: url,
+  }
+  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  data
+end
+
+scrape_list('https://zh.wikipedia.org/wiki/%E6%BE%B3%E9%96%80%E7%89%B9%E5%88%A5%E8%A1%8C%E6%94%BF%E5%8D%80%E7%AB%8B%E6%B3%95%E6%9C%83')
