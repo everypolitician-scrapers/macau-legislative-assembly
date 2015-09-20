dbf419089f8bde476e7129aabd3e7f30c0b5d803
Tony Bowden <tony@tmtm.com>
2015-09-20 10:00:52 +0100

    Not all names are in 'font' tags

--- scraper.rb
+++ scraper.rb
@@ -38,15 +38,20 @@ end
       
 def scrape_person(url)
   noko = noko_for(url)
+  name_zh_field = '中文姓名'
+  name_en_field = '葡文姓名'
+
   data = { 
     id: File.basename(url, '.*').gsub('%20','_'),
-    name__zh: noko.xpath('//table//tr[contains(.,"中文姓名")]/td[2]/span').text,
-    name__en: noko.xpath('//table//tr[contains(.,"葡文姓名")]/td[2]/font').text.tidy,
+    name__zh: noko.xpath('//table//tr[contains(.,"%s")]/td//span' % name_zh_field).text,
+    name__en: noko.xpath('//table//tr[contains(.,"%s")]/td[2]' % name_en_field).text.tidy,
     image: noko.xpath('//table//tr[contains(.,"中文姓名")]//img/@src').text,
     term: 9,
     source: url,
   }
+  # binding.pry if data[:name__en].to_s.empty? || data[:name__zh].to_s.empty?
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  # puts data
   data
 end
 
